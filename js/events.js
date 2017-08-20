e1a3c3de749a446753627da078c49da550e2bea7
wk645 <wk645@nyu.edu>
2017-08-20 16:52:02 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+function getIt() {
+	$('p').on("click", function() {
+		alert("hey!")
+	})
+}
+
+function frameIt() {
+	$('img').on("load", function() {
+		$('img').addClass('tasty')
+	})
+}
+
+function pressIt() {
+	$(document).on("keydown", function(key) {
+		if (key.which == 71) {
+			alert('s was pressed')
+		}
+	})
+}
+
+function submitIt() {
+	$("form").on("submit", function() {
+		alert("Your form is going to be submitted now.")
+		return
+	})
+}
+
 
 $(document).ready(function(){
 
-// call functions here
+	getIt()
+	frameIt()
+	pressIt()
+	submitIt()
+
 
 });
